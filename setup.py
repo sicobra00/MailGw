6a5545bd9e517ac75b72ad809d9b1822772bbfa6
sicobra00 <44222536+sicobra00@users.noreply.github.com>
2022-07-28 15:08:25 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MailGw",
+    version="0.0.81",
+    author="MainSilent",
+    description="Temporary Email",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['mail', 'email', 'temporary mail', 'temporary email', 'mailtm'],
+    url="https://github.com/MainSilent/MailTm",
+    project_urls={
+        "Bug Tracker": "https://github.com/MainSilent/MailTm/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['requests']
+)
